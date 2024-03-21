e71202d0995c6089e8bd3bd416f5469f009613ff
storialp <96848889+storialp@users.noreply.github.com>
2024-03-21 14:21:10 +0100

    Added decent hero and fixed other things

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,6 +3,7 @@ import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import { ChevronRightIcon } from '@heroicons/react/20/solid'
 import FreshLogo from './freshLogo'
+import Carlotta from './carlotta'
 
 const navigation = [
  { name: 'Home', href: '#' },
@@ -23,11 +24,6 @@ export default function Hero() {
       <a href="#" className="-m-1.5 p-1.5">
        <span className="sr-only">Your Company</span>
        <FreshLogo className="h-8 w-auto" />
-       {/* <img
-         className="h-8 w-auto"
-         src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-         alt=""
-        /> */}
       </a>
      </div>
      <div className="flex lg:hidden">
@@ -58,12 +54,13 @@ export default function Hero() {
      <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
       <div className="flex items-center justify-between">
        <a href="#" className="-m-1.5 p-1.5">
-        <span className="sr-only">Your Company</span>
-        <img
+        <span className="sr-only">Fresh by Bell Food Group</span>
+        {/* <img
          className="h-8 w-auto"
          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
          alt=""
-        />
+        /> */}
+        <FreshLogo className='h-8 w-auto' />
        </a>
        <button
         type="button"
@@ -102,33 +99,9 @@ export default function Hero() {
    </header>
 
    <div className="relative isolate pt-14">
-    <svg
-     className="absolute inset-0 -z-10 h-full w-full stroke-gray-200 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
-     aria-hidden="true"
-    >
-     <defs>
-      <pattern
-       id="83fd4e5a-9d52-42fc-97b6-718e5d7ee527"
-       width={200}
-       height={200}
-       x="50%"
-       y={-1}
-       patternUnits="userSpaceOnUse"
-      >
-       <path d="M100 200V.5M.5 .5H200" fill="none" />
-      </pattern>
-     </defs>
-     <svg x="50%" y={-1} className="overflow-visible fill-gray-50">
-      <path
-       d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z"
-       strokeWidth={0}
-      />
-     </svg>
-     <rect width="100%" height="100%" strokeWidth={0} fill="url(#83fd4e5a-9d52-42fc-97b6-718e5d7ee527)" />
-    </svg>
-    <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:flex lg:items-center lg:gap-x-10 lg:px-8 lg:py-40">
+    <div className="mx-auto max-w-7xl px-6 py-6 sm:py-32 lg:flex lg:items-center lg:gap-x-10 lg:px-8 lg:py-40">
      <div className="mx-auto max-w-2xl lg:mx-0 lg:flex-auto">
-      <div className="flex">
+      {/* <div className="flex">
        <div className="relative flex items-center gap-x-4 rounded-full px-4 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
         <span className="font-semibold text-indigo-600">We’re hiring</span>
         <span className="h-4 w-px bg-gray-900/10" aria-hidden="true" />
@@ -138,11 +111,11 @@ export default function Hero() {
          <ChevronRightIcon className="-mr-2 h-5 w-5 text-gray-400" aria-hidden="true" />
         </a>
        </div>
-      </div>
-      <h1 className="mt-10 max-w-lg text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-       A better way to ship your projects
+      </div> */}
+      <h1 className="max-w-lg text-3xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+       Welcome back Mario!
       </h1>
-      <p className="mt-6 text-lg leading-8 text-gray-600">
+      {/* <p className="mt-6 text-lg leading-8 text-gray-600">
        Esse id magna consectetur fugiat non dolor in ad laboris magna laborum ea consequat. Nisi irure aliquip
        nisi adipisicing veniam voluptate id. In veniam incididunt ex veniam adipisicing sit.
       </p>
@@ -156,10 +129,10 @@ export default function Hero() {
        <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
         Learn more <span aria-hidden="true">→</span>
        </a>
-      </div>
+      </div> */}
      </div>
-     <div className="mt-16 sm:mt-24 lg:mt-0 lg:flex-shrink-0 lg:flex-grow">
-      <svg viewBox="0 0 366 729" role="img" className="mx-auto w-[22.875rem] max-w-full drop-shadow-xl">
+     <div className="mt-2 sm:mt-24 lg:mt-0 lg:flex-shrink-0 lg:flex-grow">
+      {/* <svg viewBox="0 0 366 729" role="img" className="mx-auto w-[22.875rem] max-w-full drop-shadow-xl">
        <title>App screenshot</title>
        <defs>
         <clipPath id="2ade4387-9c63-4fc4-b754-10e687a0d332">
@@ -182,7 +155,8 @@ export default function Hero() {
        >
         <img src="https://tailwindui.com/img/component-images/mobile-app-screenshot.png" alt="" />
        </foreignObject>
-      </svg>
+      </svg> */}
+      <Carlotta className='h-64 w-auto mx-auto' />
      </div>
     </div>
    </div>
